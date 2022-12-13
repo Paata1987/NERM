8362099715e0782d4a411bbf21dffc48a59a7a54
unknown <paatatepnadze1@gmail.com>
2022-12-13 14:48:01 +0100

    add model user and login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,21 @@
 const { Router } = require('express');
+const User = require('../models/User');
 const router = Router();
+
+// /api/auth/register
+router.post('/register', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const candidate = await User.findOne({ email });
+    if (candidate) {
+      return res.status(400).json({ message: 'similar user allredy exist ' });
+    }
+  } catch (e) {
+    res.status(500).json({ message: 'somethings goes wronge' });
+  }
+});
+// /api/auth/login
+router.post('/login', async (req, res) => {});
 
 module.exports = router;
